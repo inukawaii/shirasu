46aeb7f7eae2b9f1fc6f540d3522b58b4232590d
mayumi aka inukawaii <inukawaii999@gmail.com>
2020-02-11 17:25:02 +0900

    buildやコマンド実行を早くする ・DBをpostgresに変更 ・rubyのバージョンを2.6.4に変更 ・bundlerのバージョンをver2に変更
    cf: https://techracho.bpsinc.jp/hachi8833/2019_09_06/79035

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2020_01_18_093500) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string "user_id"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
