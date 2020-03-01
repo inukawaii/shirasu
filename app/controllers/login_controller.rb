96dde5795503f6918a01ab2f351eb48e068e157e
nakanoh <murry.xxx.7@gmail.com>
2020-03-01 17:05:28 +0900

    use request params

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,6 +4,12 @@ class LoginController < ApplicationController
   end
 
   def login
+    # p user_params
     redirect_to controller: 'user', action: 'index'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_id, :password)
   end
 end
